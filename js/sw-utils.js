d4c3a68ea44bb99c27d15a8d5aadaa8d43ec8286
JOSE LUIS V <jlvillarejo@fsc.ccoo.es>
2018-11-26 12:50:07 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,18 @@
+// sw-utils
+
+// Guarda información en el caché dinámico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+	if ( res.ok ) {
+
+		return caches.open( dynamicCache ).then( cache => {
+
+			cache.put ( req, res.clone() );
+			return res.clone()
+		})
+
+	} else {
+		return res;
+	}
+
+}
